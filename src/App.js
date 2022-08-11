153da9ca54fa6f6eb4571ca39c8c859364a065c7
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-08-10 23:18:43 -0300

    Limpie el proyecto y agregué maquetado de titulo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="container text-center my-5">
+        <h1 className="display-1 text-warning">Bienvenidos</h1>
+        <p className="display-6 text-secondary">Ingresa tus tareas</p>
+      </section>
     </div>
   );
 }
